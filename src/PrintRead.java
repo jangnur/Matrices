e44925856eb195439d72663bb7eea720d4ccbc24
Nuria <jangnurgin@gmail.com>
2014-03-09 20:38:15 +0400

    update

--- src/PrintRead.java
+++ src/PrintRead.java
@@ -7,33 +7,32 @@ import java.util.Scanner;
 public class PrintRead {
 	public Matrix readMatrix (String path) throws FileNotFoundException {
 		Scanner input = new Scanner (new File(path));
-		int rows = input.nextInt();
-		int columns = input.nextInt();
+		int rows = 0;
+		int columns = 0;
 		
-		/* Комментарий здесь для меня. В файле сначала количество строк и столбцов, а затем сама матрица*/
-		/*Scanner columnReader = new Scanner(input.nextLine());
-	    while (columnReader.hasNextDouble()) {
+		Scanner columnReader = new Scanner(input.nextLine());
+	    while (columnReader.hasNextDouble() && !(columnReader.next()).equals("")) {
 	        ++columns;
 	    }
 	    columnReader.close();
 	    input.close();
 	    Scanner inputR = new Scanner (new File(path));
-		while (inputR.hasNextLine()) {
+		while (inputR.hasNextLine() && !(inputR.nextLine()).equals("")) {
 		    ++rows;
 		}
-		input.close();
-		inputR.close();*/
+		inputR.close();
+		Scanner again = new Scanner (new File(path));
 		Matrix a = new Matrix(rows, columns);
 	    double[][] x = a.getArray();
 		for (int i = 0; i < rows; ++i) {
 		    for (int j = 0; j < columns; ++j) {
-		        if (input.hasNextDouble()) {
-		            x[i][j] = input.nextDouble();
+		        if (again.hasNextDouble()) {
+		            x[i][j] = again.nextDouble();
 		        }
 		    }
 		}
 		
-		input.close();
+		again.close();
 		return a;
 	}
 	
